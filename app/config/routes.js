2abb4762fd4e23d8ffbe074af489e1cd586b98bb
Lauren Grant <lgrant1286@gmail.com>
2016-09-08 11:19:54 -0400

    Implement Home component.
    * Use placeholder image as logo.

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,12 +1,14 @@
 var React = require('react');
 var Main = require('../components/Main');
 var NavBar = require('../components/NavBar');
+var Home = require('../components/Home');
 var Router = require('react-router');
 var Route = Router.Route;
 var IndexRoute = Router.IndexRoute;
 
 module.exports = (
   <Route path="/" component={Main}>
+    <Route path="home" component={Home} />
     <IndexRoute component={NavBar} />
   </Route>
 );
