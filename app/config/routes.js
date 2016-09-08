6ef64452ac4ba93acbb66d1c15b92e2050ffe54a
Lauren Grant <lgrant1286@gmail.com>
2016-09-08 12:02:36 -0400

    Add Beer component route.

--- app/config/routes.js
+++ app/config/routes.js
@@ -2,6 +2,7 @@ var React = require('react');
 var Main = require('../components/Main');
 var NavBar = require('../components/NavBar');
 var Home = require('../components/Home');
+var Beer = require('../components/Beer');
 var Router = require('react-router');
 var Route = Router.Route;
 var IndexRoute = Router.IndexRoute;
@@ -9,6 +10,7 @@ var IndexRoute = Router.IndexRoute;
 module.exports = (
   <Route path="/" component={Main}>
     <Route path="home" component={Home} />
+    <Route path="beer" component={Beer} />
     <IndexRoute component={NavBar} />
   </Route>
 );
