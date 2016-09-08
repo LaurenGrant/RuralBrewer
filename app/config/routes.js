b7fe5d20135ff99a85b14c07327e746daf63ad0d
Lauren Grant <lgrant1286@gmail.com>
2016-09-08 12:47:13 -0400

    Add About component route.

--- app/config/routes.js
+++ app/config/routes.js
@@ -2,6 +2,7 @@ var React = require('react');
 var Main = require('../components/Main');
 var NavBar = require('../components/NavBar');
 var Home = require('../components/Home');
+var About = require('../components/About');
 var Router = require('react-router');
 var Route = Router.Route;
 var IndexRoute = Router.IndexRoute;
@@ -9,6 +10,7 @@ var IndexRoute = Router.IndexRoute;
 module.exports = (
   <Route path="/" component={Main}>
     <Route path="home" component={Home} />
+    <Route path="about" component={About} />
     <IndexRoute component={NavBar} />
   </Route>
 );
